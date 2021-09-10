9d213340da71009677ba870777275c891a905451
MuZi-Lee <57855161+Lan-Muzi@users.noreply.github.com>
2021-09-10 22:10:12 +0800

    Update 09_palindromeNumber.py

--- 09_palindromeNumber.py
+++ 09_palindromeNumber.py
@@ -1,3 +1,19 @@
+#方式一
+
+# class Solution:
+#   def isPalindrome(self, x):
+#     num = 0
+#         a = abs(x)
+#         while(a != 0):
+#             temp = a % 10
+#             num = num * 10 + temp
+#             a = int(a / 10)
+
+#         if x >= 0 and x == num:
+#             return True
+#         else:
+#             return False
+# 方式二
 class Solution:
   def isPalindrome(self, x):
     num = 0
@@ -5,7 +21,7 @@ class Solution:
         while(a != 0):
             temp = a % 10
             num = num * 10 + temp
-            a = int(a / 10)
+            a = int(a // 10)
 
         if x >= 0 and x == num:
             return True
